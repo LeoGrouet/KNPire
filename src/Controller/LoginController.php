9e94fc06249f1973e54a31331f5506626f8c63e5
LeoGrouet <leo.grouet@gmail.com>
2024-03-28 10:37:31 +0100

    chore: user sign in and login done

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,20 +4,20 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserLogType;
-use App\Form\UserType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class LoginController extends AbstractController
 {
 
     #[Route('/login', name: 'app_login')]
-    public function loginForm(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function loginForm(AuthenticationUtils $authenticationUtils, Request $request, UserRepository $repo, UserPasswordHasherInterface $passwordHasher): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -27,6 +27,14 @@ class LoginController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
+            $person = $repo->findUserByEmail($data["email"])[0];
+            $hashedpassword = $passwordHasher->isPasswordValid($person, $data["password"]);
+            if ($hashedpassword == $person->getPassword()) {
+                return $this->redirectToRoute("home");
+            } else {
+                $this->addFlash("Failure", "Raté");
+                return $this->redirectToRoute('app_login');
+            }
         }
 
         return $this->render('log/login.html.twig', [
