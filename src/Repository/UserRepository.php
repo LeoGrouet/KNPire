73a5707855e99bb1b80e66c122c1acaf8969f448
LeoGrouet <leo.grouet@gmail.com>
2024-03-28 10:37:31 +0100

    chore: user sign in and login done

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,15 +3,22 @@
 namespace App\Repository;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class UserRepository extends EntityRepository
+
+class UserRepository extends ServiceEntityRepository
 {
-    public function findUserById(int $id): array
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function findUserByEmail(string $email): array
     {
         return $this->createQueryBuilder('user')
-            ->where('user.id = :id')
-            ->setParameter('id', $id)
+            ->where('user.email = :email')
+            ->setParameter('email', $email)
             ->getQuery()
             ->getResult();
     }
