3b7d4e6be489e5e3befdced8ae0281e3d2af7ad5
Léo Grouet <111077650+LeoGrouet@users.noreply.github.com>
2024-04-04 15:15:31 +0200

    refactor: migrations because i forgot what Brice said (#8)
    * refactor: migrations because i forgot what Brice said
    
    * fix: useless comments
    
    * fix: insert description for migration

--- migrations/Version20240404123000.php
+++ migrations/Version20240404123000.php
@@ -7,27 +7,24 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-
-final class Version20240404075931 extends AbstractMigration
+final class Version20240404123000 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Task table creation';
+        return 'Table Task and User creation';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(180) NOT NULL, description VARCHAR(255) NOT NULL, points INT NOT NULL, UNIQUE INDEX UNIQ_527EDB255E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD task_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, task_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D6498DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6498DB60186 FOREIGN KEY (task_id) REFERENCES task (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6498DB60186 ON user (task_id)');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6498DB60186');
         $this->addSql('DROP TABLE task');
-        $this->addSql('DROP INDEX UNIQ_8D93D6498DB60186 ON user');
-        $this->addSql('ALTER TABLE user DROP task_id');
+        $this->addSql('DROP TABLE user');
     }
 }
