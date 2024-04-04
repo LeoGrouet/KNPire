2aad9855bb4fdc6ade397efc765a1581b40c6736
LeoGrouet <leo.grouet@gmail.com>
2024-04-04 14:11:03 +0200

    style: TaskForm type change and add SVG for logo

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -20,7 +21,7 @@ class TaskType extends AbstractType
             )
             ->add(
                 'description',
-                TextType::class
+                TextareaType::class
             )
             ->add(
                 'points',
