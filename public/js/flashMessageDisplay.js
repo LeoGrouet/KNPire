ee40483b6f1e137ca443939f3e2bc186f5770c83
LeoGrouet <leo.grouet@gmail.com>
2024-04-08 09:53:34 +0200

    style: add animation for flashMessage

--- public/js/flashMessageDisplay.js
+++ public/js/flashMessageDisplay.js
@@ -1,7 +1,11 @@
 const message = document.querySelector(".flash-message");
-
+const container = document.querySelector(".flash-message-container");
 if (message) {
   setTimeout(() => {
-    message.style.display = "none";
+    message.classList.add("animate__animated", "animate__lightSpeedOutLeft");
+    container.style.height = "0px";
   }, 2000);
+  setTimeout(() => {
+    message.style.display = "none";
+  }, 4000);
 }
